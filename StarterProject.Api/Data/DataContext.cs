ccb7c6696b7c0047a27e05dc2616669ba563f72f
Matthew Puneky <matthew.puneky@gmail.com>
2019-02-15 17:59:22 -0600

    feat(api): basically the whole thing

---
+++ StarterProject.Api/Data/DataContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StarterProject.Api.Common;
+using StarterProject.Api.Features.Users;
+using StarterProject.Api.Security;
+
+namespace StarterProject.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var passwordHasher = new PasswordHash("admin");
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 1,
+                    Role = Constants.Users.Roles.Admin,
+                    Username = "admin",
+                    Email = "admin@admin.com",
+                    FirstName = "Seeded-Admin-FirstName",
+                    LastName = "Seeded-Admin-LastName",
+                    PasswordHash = passwordHasher.Hash,
+                    PasswordSalt = passwordHasher.Salt
+                });
+        }
+    }
+}
